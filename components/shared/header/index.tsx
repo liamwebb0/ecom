a7277450e1f105a70045a04842dd2bc96e1d0ba8
liamwebb0 <liam.wweebb@gmail.com>
2025-10-26 19:38:28 +0000

    feat: added footer and index pages

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,6 +1,11 @@
 import { APP_NAME } from '@/lib/constants';
 import Image from 'next/image';
 import Link from 'next/link';
+import Search from './search';
+import Menu from './menu';
+import { Button } from '@/components/ui/button';
+import { MenuIcon } from 'lucide-react';
+import data from '@/lib/data';
 
 export default function Header() {
   return (
@@ -18,8 +23,36 @@ export default function Header() {
                 height={40}
                 alt={`${APP_NAME} logo`}
               />
+              {APP_NAME}
             </Link>
           </div>
+          <div className='hidden md:block flex-1 max-w-xl'>
+            <Search />
+          </div>
+          <Menu />
+        </div>
+        <div className='md:hidden block py-2'>
+          <Search />
+        </div>
+      </div>
+      <div className='flex items-center px-3 mb-[1px] bg-gray-800'>
+        <Button
+          variant='ghost'
+          className='header-button flex items-center gap-1 text-base [&_svg]:size-6'
+        >
+          <MenuIcon />
+          All
+        </Button>
+        <div className='flex items-center flex-wrap gap-3 overflow-hidden max-h-[42px]'>
+          {data.headerMenus.map((menu) => (
+            <Link
+              href={menu.href}
+              key={menu.href}
+              className='header-button !p-2'
+            >
+              {menu.name}
+            </Link>
+          ))}
         </div>
       </div>
     </header>
