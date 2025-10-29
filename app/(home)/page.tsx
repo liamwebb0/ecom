151186c9a846c3ceca63460418d71d0ad089da45
liamwebb0 <liam.wweebb@gmail.com>
2025-10-29 17:19:16 +0000

    feat: added carousel to page.tsx

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,4 +1,5 @@
 import { HomeCarousel } from '@/components/shared/home/home-carousel';
+import data from '@/lib/data';
 
 export default async function Page() {
   return <HomeCarousel items={data.carousels} />;
